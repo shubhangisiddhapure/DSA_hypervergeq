9a5d9c2b6221567393773d8d826121af575ad601
shubhangisiddhapure <79439864+shubhangisiddhapure@users.noreply.github.com>
2021-04-06 08:10:34 +0530

    Add files via upload

---
+++ step9/stack2_queue.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+    def enQueue(self, x):
+        while len(self.s1) != 0: 
+            self.s2.append(self.s1[-1]) 
+            self.s1.pop()
+        self.s1.append(x) 
+        while len(self.s2) != 0: 
+            self.s1.append(self.s2[-1]) 
+            self.s2.pop()
+    def deQueue(self):
+        if len(self.s1) == 0: 
+            print("Q is Empty")
+        x = self.s1[-1] 
+        self.s1.pop() 
+        return x
+q = Queue()
+q.enQueue(22) 
+q.enQueue(23) 
+q.enQueue(32) 
+  
+print(q.deQueue())
+print(q.deQueue())
+print(q.deQueue())
